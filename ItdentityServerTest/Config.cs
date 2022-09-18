258895dabff70d77b427c3f1969455ebbc26319f
Dishant Rana <ranadishant111@gmail.com>
2022-09-18 20:05:05 +0530

    first commit

---
+++ ItdentityServerTest/Config.cs
@@ -0,0 +1,38 @@
+﻿
+using IdentityServer4.Models;
+
+namespace ItdentityServerTest
+{
+    public static class Config
+    {
+
+        public static IEnumerable<ApiScope> ApiScopes => new[]
+        {
+            new ApiScope("demoapi.read"),
+            new ApiScope("demoapi.write"),
+        };
+        
+        public static IEnumerable<Client> Clients => new[]
+        {
+            new Client()
+            {
+                ClientId = "CapServer",
+                ClientName = " cap identitiy server",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                ClientSecrets = { new Secret("MyClientSecret".Sha256())},
+                AllowedScopes = { "demoapi.read", "demoapi.write" }
+            }
+        };
+        public static IEnumerable<ApiResource> ApiResources=> new[]
+        {
+            new ApiResource("demoapi")
+            {
+             Scopes = { "demoapi.read", "demoapi.write" },
+             ApiSecrets = { new Secret("ScopeSecret".Sha256())},
+             UserClaims = { "roles"}
+            }
+        };
+
+
+    }
+}
---
+++ ItdentityServerTest/Program.cs
@@ -0,0 +1,34 @@
+using ItdentityServerTest;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(Config.Clients)
+    .AddInMemoryApiResources(Config.ApiResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddDeveloperSigningCredential();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseIdentityServer();
+
+app.MapControllers();
+
+app.Run();
